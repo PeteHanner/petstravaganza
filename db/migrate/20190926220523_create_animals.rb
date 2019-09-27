d3ad5b74de9149c67473aacf3fe4823d369d8957
Pete Hanner <petehanner@gmail.com>
2019-09-26 20:14:46 -0400

    database setup

---
+++ db/migrate/20190926220523_create_animals.rb
@@ -0,0 +1,11 @@
+class CreateAnimals < ActiveRecord::Migration[6.0]
+  def change
+    create_table :animals do |t|
+      t.string :name
+      t.string :image
+      t.string :species
+
+      t.timestamps
+    end
+  end
+end
