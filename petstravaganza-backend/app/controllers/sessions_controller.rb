3e38cd54b998ada3f6935e23dd609ac0506fc4c8
Pete Hanner <petehanner@gmail.com>
2019-09-27 12:03:31 -0400

    minor controller refactor

--- petstravaganza-backend/app/controllers/sessions_controller.rb
+++ petstravaganza-backend/app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def index
     session_animals = Animal.generate_animal_array
-    task_list = Task.task_list(session_animals)
-    render json:task_list
+    # task_list = Task.task_list(session_animals)
+    render json:session_animals, only: [:name, :species, :image]
   end
 end
