87853416147a7c4693eedf7de5c2ee6d9d814ef0
Pete Hanner <petehanner@gmail.com>
2019-10-01 11:35:24 -0400

    no duplicate tasks

--- petstravaganza-backend/app/models/task.rb
+++ petstravaganza-backend/app/models/task.rb
@@ -3,20 +3,50 @@ class Task < ApplicationRecord
 
   @task_choices = ['food', 'water', 'potty', 'exercise']
 
-  # animals = self.get_animals
-
   def self.task_list(animal_array)
+    # initialize empty array
     task_array = []
-    # X times create a task
+    # populate array with 15 tasks
     15.times do
-      animal_instance = animal_array.sample
-      task_array << self.create(
-        activity: @task_choices.sample,
+      task_array << self.make_task(animal_array)
+    end
+
+    # remove duplicates and top off until 15 unique tasks
+    loop do
+      check = self.check_for_duplicates(task_array)
+      task_array = check[0]
+      diff = check[1]
+      break if diff <= 0
+      diff.times {task_array << self.make_task(animal_array)}
+    end
+
+    # initialize, populate, and return array for actually created Tasks
+    json_return = []
+    task_array.each do |task|
+      json_return << Task.create(
+        activity: task.activity,
+        animal: task.animal,
         duration: 10000,
-        status: 'queued',
-        animal: Animal.find(animal_instance['id'])
       )
     end
-    task_array
+    json_return
+  end
+
+  # Make a new task without saving to database
+  def self.make_task(animal_array)
+    animal_instance = animal_array.sample
+    self.new(
+      activity: @task_choices.sample,
+      animal: Animal.find(animal_instance['id'])
+    )
+  end
+
+  def self.check_for_duplicates(task_array)
+    # filter out tasks with same animal and activity
+    no_dupes = task_array.uniq { |task| [task.animal, task.activity] }
+    # store number of duplicates that were removed
+    diff = task_array.length - no_dupes.length
+    # pass array of unique tasks and difference for further work
+    return [no_dupes, diff]
   end
 end
